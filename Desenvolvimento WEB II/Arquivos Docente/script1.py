9ab4208181556333e58b9afea0996db052e77854
Bruno Campagnol <bruno_unky@hotmail.com>
2024-02-24 10:52:11 -0300

    Commit

---
+++ Desenvolvimento WEB II/Arquivos Docente/script1.py
@@ -0,0 +1,39 @@
+# Sequencia
+print('oi mundo')
+
+print('passo 2')
+
+# Seleção ( IF )
+valor1 = 5
+
+if valor1 > 10:
+    print("Valor maior que dez")
+
+if valor1 > 2:
+    print("Valor maior que dois")
+
+# Iterações com objeto iterável
+lista = list(range(1, 10))
+
+for irineu in lista:
+    print("Achei: ")
+    print(irineu)
+    
+
+lista = list(range(1,10))
+lista.append('Leite')
+lista.append(list(range(1,10)))
+lista.append('Café')
+
+# Desafio 1
+# Exibir via print somente os elementos que sejam do tipo string
+print("Encontrando elementos string na lista  \n\n")
+for quadradinho in lista:
+    if type(quadradinho) == str:
+        print(quadradinho)
+
+# Desafio 2
+# Dado uma string qualquer, separar as letras com o caracter pipe (|)
+# Exemplo:
+# Fatec => F | A | T | E | C
+ 
