3820eafa88541b0a73983015643f6e74a7b5c4bd
Bruno Campagnol <bruno_unky@hotmail.com>
2024-02-28 21:16:45 -0300

    Import domin App.java

--- Tecnica de Programacao II/Desenvolvimento em Aula/poo_review/src/App.java
+++ Tecnica de Programacao II/Desenvolvimento em Aula/poo_review/src/App.java
@@ -1,4 +1,8 @@
-import domain.*;
+import domain.Pikachu;
 
-public class 
+public class App {
+    public static void main(String[] args) throws Exception {
+        Pikachu pikachu = new Pikachu("Pikachu", 1500, 112);
+        System.out.println(pikachu);
+    }
 }
