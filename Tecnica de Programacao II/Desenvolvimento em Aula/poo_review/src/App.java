c465c6a23ff1db727cf0390d649fc4c0e099c13b
Bruno Campagnol <bruno_unky@hotmail.com>
2024-02-28 21:59:58 -0300

    Aula 1

--- Tecnica de Programacao II/Desenvolvimento em Aula/poo_review/src/App.java
+++
@@ -1,8 +0,0 @@
-import domain.Pikachu;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Pikachu pikachu = new Pikachu("Pikachu", 1500, 112);
-        System.out.println(pikachu);
-    }
-}
