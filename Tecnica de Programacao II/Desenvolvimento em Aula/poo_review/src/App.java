92a652c8af5e62de24b61c77b8f021bb422cc440
Bruno Campagnol <bruno_unky@hotmail.com>
2024-03-06 20:56:11 -0300

    Att

--- Tecnica de Programacao II/Desenvolvimento em Aula/poo_review/src/App.java
+++ Tecnica de Programacao II/Desenvolvimento em Aula/poo_review/src/App.java
@@ -1,13 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import domain.Charmander;
 import domain.Pikachu;
 import domain.Pokemon;
+import domain.Squirtle;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Charmander charmander = new Charmander("Charmander", 1500, 112);
-        System.out.println(charmander);
-        charmander.iChooseYou();
+        Pikachu pikachu = new Pikachu("Pikachu", 1500, 112);
+        Charmander charmander = new Charmander("Pikachu", 800, 90);
+        Squirtle squirtle = new Squirtle("Pikachu", 790, 87);
+
+        List<Pokemon> pokemons = Arrays.asList(pikachu, charmander, squirtle);
+
+            for (Pokemon pokemon : pokemons){
+                pokemon.attack();
+            }
     }
 }
