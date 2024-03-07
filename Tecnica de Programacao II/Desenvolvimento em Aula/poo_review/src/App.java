9e78794628fafbc57d26b00b80efbc608029d705
Bruno Campagnol <bruno_unky@hotmail.com>
2024-03-06 21:54:16 -0300

    Att

--- Tecnica de Programacao II/Desenvolvimento em Aula/poo_review/src/App.java
+++ Tecnica de Programacao II/Desenvolvimento em Aula/poo_review/src/App.java
@@ -7,16 +7,20 @@ import domain.Charmander;
 import domain.Pikachu;
 import domain.Pokemon;
 import domain.Squirtle;
+import domain.Zapdos;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Pikachu pikachu = new Pikachu("Pikachu", 1500, 112);
         Charmander charmander = new Charmander("Pikachu", 800, 90);
         Squirtle squirtle = new Squirtle("Pikachu", 790, 87);
+        Zapdos zapdos = new Zapdos("Zapdos", 790, 87);
 
-        List<Pokemon> pokemons = Arrays.asList(pikachu, charmander, squirtle);
+        List<Pokemon> pokemons = Arrays.asList(pikachu, charmander, squirtle, zapdos);
 
             for (Pokemon pokemon : pokemons){
+                System.out.println();
+                System.out.println(pokemon);
                 pokemon.attack();
             }
     }
