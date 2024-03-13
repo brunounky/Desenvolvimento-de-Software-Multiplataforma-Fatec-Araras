19c7055abc5229c78659f10242ed9939add3bd9c
Bruno Campagnol <bruno_unky@hotmail.com>
2024-03-13 20:41:49 -0300

    Att Jokenpo

--- Tecnica de Programacao II/Jokenpo/main.cpp
+++ Tecnica de Programacao II/Jokenpo/main.cpp
@@ -16,6 +16,8 @@ int main(int argc, char** argv) {
 	cout << "1 - Pedra" << endl;
 	cout << "2 - Papel" << endl;
 	cout << "3 - Tesoura" << endl;
+	cout << "4 - Lagarto" << endl;
+	cout << "5 - Spock" << endl;
 	cin >> op;
 	
 	if (op == 1){
@@ -24,10 +26,14 @@ int main(int argc, char** argv) {
 		cout << endl << "Voce escolheu Papel";
 	}if (op == 3){
 		cout << endl << "Voce escolheu Tesoura";
+	}if (op == 4){
+		cout << endl << "Voce escolheu Lagarto";
+	}if (op == 5){
+		cout << endl << "Voce escolheu Spock";
 	}
 	
 	
-	opPC = rand() % 3;
+	opPC = rand() % 5;
 	
 	if (opPC == 1){
 		cout << endl << "Computador escolheu Pedra";
@@ -35,6 +41,10 @@ int main(int argc, char** argv) {
 		cout << endl << "Computador escolheu Papel";
 	}if (opPC == 3){
 		cout << endl << "Computador escolheu Tesoura";
+	}if (opPC == 4){
+		cout << endl << "Computador escolheu Lagarto";
+	}if (opPC == 5){
+		cout << endl << "Computador escolheu Spock";
 	}
 	
 	cout << endl;
@@ -51,6 +61,17 @@ int main(int argc, char** argv) {
 		cout << endl << "Voce venceu";
 		cout << endl << "pedra esmaga tesoura";
 	}
+	if (op == 1 and opPC == 4){
+		cout << endl << "Voce venceu";
+		cout << endl << "pedra esmaga lagarto";
+	}
+	if (op == 1 and opPC == 5){
+		cout << endl << "Voce venceu";
+		cout << endl << "Spock vaporiza pedra";
+	}
+	
+	
+	
 	if (op == 2 and opPC == 1){
 		cout << endl << "Voce venceu";
 		cout << endl << "Papel embrulha pedra";
@@ -63,6 +84,17 @@ int main(int argc, char** argv) {
 		cout << endl << "Computador venceu";
 		cout << endl << "Tesoura corta papel";
 	}
+	if (op == 2 and opPC == 4){
+		cout << endl << "Computador venceu";
+		cout << endl << "Lagarto come papel";
+	}
+	if (op == 2 and opPC == 5){
+		cout << endl << "Voce venceu";
+		cout << endl << "Papel refuta spock";
+	}
+	
+	
+	
 	if (op == 3 and opPC == 1){
 		cout << endl << "Computador venceu";
 		cout << endl << "Pedra esmaga tesoura";
@@ -75,6 +107,58 @@ int main(int argc, char** argv) {
 		cout << endl << "Não temos um vencedor, jogue dnv";
 		cout << endl << "Tanto o computador quanto voce escolheram tesoura";
 	}
+	if (op == 3 and opPC == 4){
+		cout << endl << "Voce venceu";
+		cout << endl << "Tesoura decapta lagarto";
+	}
+	if (op == 3 and opPC == 5){
+		cout << endl << "Computador venceu";
+		cout << endl << "Spock derrete tesoura";
+	}
 	
+	
+	
+	if (op == 4 and opPC == 1){
+		cout << endl << "Computador venceu";
+		cout << endl << "Pedra esmaga lagrato";
+	}
+	if (op == 4 and opPC == 2){
+		cout << endl << "Voce venceu";
+		cout << endl << "Lagarto come papel";
+	}
+	if (op == 4 and opPC == 3){
+		cout << endl << "Computador venceu";
+		cout << endl << "Tesoura decapta lagarto";
+	}
+	if (op == 4 and opPC == 4){
+		cout << endl << "Não temos um vencedor, jogue dnv";
+		cout << endl << "Tanto o computador quanto voce escolheram tesoura";
+	}
+	if (op == 4 and opPC == 5){
+		cout << endl << "Voce venceu";
+		cout << endl << "Lagarto envenena spock";
+	}
+	
+	
+	if (op == 5 and opPC == 1){
+		cout << endl << "Voce venceu";
+		cout << endl << "Spock vaporiza pedra";
+	}
+	if (op == 5 and opPC == 2){
+		cout << endl << "Computador venceu";
+		cout << endl << "Papel refuta spock";
+	}
+	if (op == 5 and opPC == 3){
+		cout << endl << "Voce venceu";
+		cout << endl << "Spock derrete tesoura";
+	}
+	if (op == 5 and opPC == 4){
+		cout << endl << "Computador venceu";
+		cout << endl << "Lagarto envenena spock";
+	}
+	if (op == 5 and opPC == 5){
+		cout << endl << "Não temos um vencedor, jogue dnv";
+		cout << endl << "Tanto o computador quanto voce escolheram tesoura";
+	}
 	return 0;
 }
